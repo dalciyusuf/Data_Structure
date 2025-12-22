491851d78d7a8d33fab13ce84fb5612012e24df0
Yusuf <dalciyusuf@gmail.com>
2025-12-22 16:33:32 +0300

    Hanoi

---
+++ 2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void hanoi(int n, char kaynak, char hedef, char yardimci) {
+    
+   
+    if (n == 1) {
+        printf("Disk 1 tasindi: %c -> %c\n", kaynak, hedef);
+        return;
+    }
+
+    hanoi(n - 1, kaynak, yardimci, hedef);
+
+    
+    printf("Disk %d tasindi: %c -> %c\n", n, kaynak, hedef);
+
+
+    hanoi(n - 1, yardimci, hedef, kaynak);
+}
+
+int main() {
+    int diskSayisi;
+
+    printf("--- Hanoi Kuleleri Cozumu ---\n");
+    printf("Disk sayisini giriniz: ");
+    scanf("%d", &diskSayisi);
+    
+
+    printf("\n%d disk icin cozum adimlari:\n", diskSayisi);
+    printf("------------------------------\n");
+
+    hanoi(diskSayisi, 'A', 'C', 'B');
+
+    return 0;
+}
